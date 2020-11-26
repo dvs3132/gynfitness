5eccf50b8b44f06c574c584ff5ad74b32bcabf41
dvs3132 <dvs3132@gmail.com>
2020-11-26 17:00:31 -0600

    agregando el menu para dispositivos moviles

--- wp-content/themes/ginfitness/functions.php
+++ wp-content/themes/ginfitness/functions.php
@@ -14,12 +14,18 @@ function gynfitness_scripts_styles(){
 
     wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
 
+    wp_enqueue_style('slicknavCSS', get_template_directory_uri() . '/css/slicknav.min.css', array(), '1.0.0');
+
     wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:wght@400;700;900&family=Staatliches&display=swap', array(), '1.0.0');
 
     // la hoja de estilos principal 
     wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'googleFont'), '1.0.0');
 
-    
+    // Dependencias Javascript
+    wp_enqueue_script('slicknavJS', get_template_directory_uri() . '/js/jquery.slicknav.min.js', array('jquery'), '1.0.0', true);
+
+    wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0', true); 
+
 }
 
 add_action('wp_enqueue_scripts', 'gynfitness_scripts_styles');

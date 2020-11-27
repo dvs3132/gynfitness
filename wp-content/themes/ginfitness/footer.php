ff63d8083cb1a226f2ad0cb2506aff33712c29d4
dvs3132 <dvs3132@gmail.com>
2020-11-26 18:35:55 -0600

    Mostrando informacion en el Footer

--- wp-content/themes/ginfitness/footer.php
+++ wp-content/themes/ginfitness/footer.php
@@ -1,6 +1,17 @@
 
-            <footer class="site-footer">
-                <p>Site Footer</p>
+            <footer class="site-footer contenedor">
+                <hr>
+
+                <?php
+                $args = array(
+                    'theme_location' => 'menu-principal',
+                    'container' => 'nav',
+                    'container_class' => 'menu-principal'
+                );
+                wp_nav_menu($args);
+                ?>
+
+                <p class="copyrigth">Todos los derechos reservados <?php echo get_bloginfo('name') . " " . date('Y'); ?> </p>
             </footer>
 
             <?php wp_footer(); ?>

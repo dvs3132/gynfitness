bba45bb6c7b01e4ad146965ee97891b7b2b8a613
dvs3132 <dvs3132@gmail.com>
2020-11-26 19:05:15 -0600

    Agregando estilos al footer

--- wp-content/themes/ginfitness/footer.php
+++ wp-content/themes/ginfitness/footer.php
@@ -2,16 +2,19 @@
             <footer class="site-footer contenedor">
                 <hr>
 
-                <?php
-                $args = array(
-                    'theme_location' => 'menu-principal',
-                    'container' => 'nav',
-                    'container_class' => 'menu-principal'
-                );
-                wp_nav_menu($args);
-                ?>
+                <div class="contenido-footer">
+                    <?php
+                    $args = array(
+                        'theme_location' => 'menu-principal',
+                        'container' => 'nav',
+                        'container_class' => 'menu-principal'
+                    );
+                    wp_nav_menu($args);
+                    ?>
 
-                <p class="copyrigth">Todos los derechos reservados <?php echo get_bloginfo('name') . " " . date('Y'); ?> </p>
+                    <p class="copyrigth">Todos los derechos reservados <?php echo get_bloginfo('name') . " " . date('Y'); ?> </p>
+                    
+                </div>
             </footer>
 
             <?php wp_footer(); ?>

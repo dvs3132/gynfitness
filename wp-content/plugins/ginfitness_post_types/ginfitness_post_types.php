550dd1a67b285ae97153aa355324f6801cea7688
dvs3132 <dvs3132@gmail.com>
2020-12-02 10:41:38 -0600

    Estructura basica para Plugins personalizados

---
+++ wp-content/plugins/ginfitness_post_types/ginfitness_post_types.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+    Plugin Name: Gym Fitness - Post Type
+    Plugin URI:
+    Description: Añade Post Type al sitio de Gym Fitness
+    Version: 1.0.0
+    Author: Purple Logistic
+    Author URI:
+    Text Domain: Gymfitness
+*/

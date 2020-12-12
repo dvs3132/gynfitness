ee07595a9c8165007265d6c2780668b1502c7543
dvs3132 <dvs3132@gmail.com>
2020-12-11 19:07:17 -0600

    Estilos CSS para las clases view Web

--- wp-content/themes/ginfitness/page-nuestras-clases.php
+++ wp-content/themes/ginfitness/page-nuestras-clases.php
@@ -2,11 +2,11 @@
 <?php get_header();?>
 
     <main class="contenedor pagina seccion no-sidebar">
-        <div class="contenido-principal text-center">
+        <div class="text-center">
             <?php get_template_part('template-parts/paginas'); ?>
 
             <?php  gymfitness_lista_clases(); ?>
-            
+
         </div>
 
     </main>

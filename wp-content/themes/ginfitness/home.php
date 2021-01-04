b20d39b1f221074139bf08cf276eae9367d54e51
dvs3132 <dvs3132@gmail.com>
2021-01-04 10:04:13 -0600

    Creando el template y loop para la seccion "blog"

---
+++ wp-content/themes/ginfitness/home.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+    <main class="pagina seccion no-sidebars contenedor">
+        <ul class="listado-blog">
+            <?php while(have_posts()): the_post(); ?>
+                <li class="card gradient">
+                    <?php the_post_thumbnail('mediano'); ?>
+                </li>
+            <?php endwhile; ?>    
+        </ul>
+    </main>
+
+
+<?php get_footer(); ?>
--- wp-content/themes/ginfitness/index.php
+++ wp-content/themes/ginfitness/index.php
@@ -1,2 +1,4 @@
 
 <?php get_header();?>
+
+<h1>Index.php</h1>

d8e59c86be13413e505bef7e59a52a7f18ca24a7
dvs3132 <dvs3132@gmail.com>
2021-01-07 16:37:56 -0600

    Agregando Leaftlet

--- wp-content/themes/ginfitness/functions.php
+++ wp-content/themes/ginfitness/functions.php
@@ -42,6 +42,10 @@ function gynfitness_scripts_styles(){
     wp_enqueue_style('lightboxCSS', get_template_directory_uri() . '/css/lightbox.min.css', array(), '2.11.0');
     endif;
 
+    if(is_page('contacto')):
+        wp_enqueue_style('leaftletCSS', 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css', array(), '1.7.1');
+        endif;
+
     // la hoja de estilos principal 
     wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'googleFont'), '1.0.0');
 
@@ -51,6 +55,10 @@ function gynfitness_scripts_styles(){
     if(is_page('galeria')):
     wp_enqueue_script('lightboxJS', get_template_directory_uri() . '/js/lightbox.min.js', array('jquery'), '2.11.0', true);
     endif;
+
+    if(is_page('contacto')):
+        wp_enqueue_script('leaftletJS', 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js', array(), '1.7.1', true);
+        endif;
 
     wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0', true); 
 
